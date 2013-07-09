dc94923fe20dd4b1739759bbcfcfccfd1398d0db
malinda suhash <malinda.suhash@gmail.com>
2013-07-09 22:34:44 +0100

    Adding updated proj file and assembly info

---
+++ InheritanceCanBeBad.Test/Stubs/DatabaseStub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InheritanceCanBeBad.Test.Stubs
+{
+    /// <summary>
+    /// Simple stub for <see cref="IDatabase"/>
+    /// </summary>
+    public class DatabaseStub : IDatabase
+    {
+        public Action<string> StoreAction { get; set; }
+
+        public void Store(string data)
+        {
+            if (StoreAction != null)
+            {
+                StoreAction(data);
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InheritanceCanBeBad/StoreInSqlDatabaseForTesting.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InheritanceCanBeBad
+{
+    /// <summary>
+    /// Refactored <see cref="StoreInDatabase"/> that is suitable for unit testing.
+    /// </summary>
+    /// <remarks>
+    /// Crazy!
+    /// </remarks>
+    public class StoreInSqlDatabaseForTesting : StoreInDatabase
+    {
+        private readonly Action<IEnumerable<string>> _addAction;
+
+        public StoreInSqlDatabaseForTesting(IDatabase database)
+            : this(database, null)
+        {
+            _addAction = Add;
+        }
+
+        /// <summary>
+        /// Ctor just to support unit testing.
+        /// </summary>
+        internal StoreInSqlDatabaseForTesting(IDatabase database, Action<IEnumerable<string>> addAction)
+            : base(database)
+        {
+            _addAction = addAction;
+        }
+
+        public override void Add()
+        {
+            var valuesToStore = new List<string>();
+
+            valuesToStore.AddRange(new [] { "A", "B", "C" });
+
+            _addAction(valuesToStore);
+        }
+    }
+}
